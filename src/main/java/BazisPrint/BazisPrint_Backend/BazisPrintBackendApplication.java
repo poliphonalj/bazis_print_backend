0eee804912db5436edba32389f0f8823736a548b
gabort07 <gabort07@hotmail.com>
2021-02-11 15:37:43 +0100

    first commit

---
+++ src/main/java/BazisPrint/BazisPrint_Backend/BazisPrintBackendApplication.java
@@ -0,0 +1,13 @@
+package BazisPrint.BazisPrint_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazisPrintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazisPrintBackendApplication.class, args);
+	}
+
+}
